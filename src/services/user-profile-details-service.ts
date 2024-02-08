236b7037624dc21e868b0ab2bc62d50cd5c838e6
danielkatz12 <danielkatz1212@gmail.com>
2024-02-08 23:32:34 +0200

    create user-profile-details-service

--- src/services/user-profile-details-service.ts
+++ src/services/user-profile-details-service.ts
@@ -7,11 +7,54 @@ export const saveNewUserProfileDetails = (newUserProfileDetails: UserDetailsData
     return new Promise<UserDetailsData>((resolve, reject) => {
         console.log("Saving new user profile details...")
         console.log(newUserProfileDetails)
-        apiClient.post("/user.profile.details", newUserProfileDetails, axiosConfig).then((response) => {
-            console.log("save on then: ", response)
+        apiClient.post("/user-profile-details", newUserProfileDetails, axiosConfig).then((response) => {
+            console.log("saved user-profile details successfully: ", response)
             resolve(response.data)
         }).catch((error) => {
-            console.log("save on catch: ", error)
+            console.log("failed to save user-profile details: ", error)
+            reject(error)
+        })
+    })
+
+}
+
+export const updateUserProfileDetails = (userProfileDetailsToUpdate: UserDetailsData) => {
+    return new Promise<UserDetailsData>((resolve, reject) => {
+        console.log("Updating user profile details...")
+        console.log(userProfileDetailsToUpdate);
+        apiClient.put(`/user-profile-details/user/${userProfileDetailsToUpdate.user}`, userProfileDetailsToUpdate, axiosConfig).then((response) => {
+            console.log("updated user-profile details successfully: ", response)
+            resolve(response.data)
+        }).catch((error) => {
+            console.log("failed to update user-profile details: ", error)
+            reject(error)
+        })
+    })
+
+}
+
+export const getAllUserProfileDetails = () => {
+    return new Promise<UserDetailsData[]>((resolve, reject) => {
+        console.log("get all user profile details...")
+        apiClient.get(`/user-profile-details/`, axiosConfig).then((response) => {
+            console.log("get all user profile details successfully: ", response)
+            resolve(response.data)
+        }).catch((error) => {
+            console.log("failed to get all user profile details: ", error)
+            reject(error)
+        })
+    })
+
+}
+
+export const getMyUserProfileDetails = (userId: string) : Promise<UserDetailsData> => {
+    return new Promise<UserDetailsData>((resolve, reject) => {
+        console.log("get My user profile details...")
+        apiClient.get(`/user-profile-details/user/${userId}`, axiosConfig).then((response) => {
+            console.log("got My user profile details successfully: ", response)
+            resolve(response.data)
+        }).catch((error) => {
+            console.log("failed to get all user profile details: ", error)
             reject(error)
         })
     })
