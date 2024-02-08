66684fe02ab8424c7dc84c6173e5c073616328a2
danielkatz12 <danielkatz1212@gmail.com>
2024-02-08 23:28:43 +0200

    creating posts-service

---
+++ src/dtos/post-dto.ts
@@ -0,0 +1,9 @@
+export interface PostDto {
+    _id?: string;
+    city: string;
+    street: string;
+    streetNumber: number;
+    description: string;
+    imageUrl: string;
+    pricePerDay: number;
+}
--- src/services/posts-service.ts
+++ src/services/posts-service.ts
@@ -1,13 +1,40 @@
-import apiClient, { CanceledError } from "./api-client"
+import apiClient, {axiosConfig, CanceledError} from "./api-client"
 
-import { PostData } from "../Post"
+import {PostItemData} from "../components/PostItem.tsx"
+import {PostDto} from "../dtos/post-dto.ts";
+import {UserDetailsData} from "../stateManagement/RecoilState.ts";
 
-export { CanceledError }
-const getAllPosts = () => {
+export {CanceledError}
+
+export function getAllFullPosts() {
     const abortController = new AbortController()
-    const req = apiClient.get<PostData[]>('studentpost', { signal: abortController.signal })
-    return { req, abort: () => abortController.abort() }
-
+    const req = apiClient.get<PostItemData[]>('post/get-all', {signal: abortController.signal})
+    return {req, abort: () => abortController.abort()}
 }
 
-export default { getAllPosts }
+export function insertPost(postDto: PostDto): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+        console.log("inserting new post")
+        console.log(postDto)
+        apiClient.post("/post", postDto, axiosConfig).then((response) => {
+            console.log("Post saved successfully: ", response)
+            resolve();
+        }).catch((error) => {
+            console.log("failed to insert Post: ", error)
+            reject(error)
+        })
+    })
+}
+
+export function updatePost(postDto: PostDto): Promise<PostDto> {
+    return new Promise<PostDto>((resolve, reject) => {
+        console.log("Updating post: ", postDto);
+        apiClient.put(`/post/${postDto._id}`, postDto, axiosConfig).then((response) => {
+            console.log("Post saved successfully: ", response)
+            resolve(response.data);
+        }).catch((error) => {
+            console.log("failed to update Post: ", error)
+            reject(error)
+        })
+    })
+}
