313610c25b41b41faf6b6a12be3586105609fe29
danielkatz12 <danielkatz1212@gmail.com>
2024-02-08 23:27:35 +0200

    creating file service

--- src/services/file-service.ts
+++ src/services/file-service.ts
@@ -1,8 +1,9 @@
-import apiClient from "./api-client";
+import apiClient, {axiosConfig} from "./api-client";
 
 interface IUpoloadResponse {
     url: string;
 }
+
 export const uploadPhoto = async (photo: File) => {
     return new Promise<string>((resolve, reject) => {
         console.log("Uploading photo..." + photo)
@@ -22,5 +23,30 @@ export const uploadPhoto = async (photo: File) => {
             });
         }
     });
+}
+
+export const deletePhoto = async (imageUrl: string) => {
+    return new Promise<void>((resolve, reject) => {
+        console.log("Deleting photo with the urs: " + imageUrl);
+        apiClient.delete("/file/delete-file?fileUrl=" + encodeURIComponent(imageUrl)).then((response) => {
+            console.log("Photo deleted successfully: ", response)
+            resolve();
+        }).catch((error) => {
+            console.log("failed to delete photo: ", error)
+            reject(error)
+        })
+    })
+}
+
+export const updatePhoto = async (oldImageUrl: string, newImageFile: File) => {
+    try {
+        await deletePhoto(oldImageUrl); // Delete old photo
+        const newImageUrl = await uploadPhoto(newImageFile); // Upload new photo
+        console.log("Image updated successfully");
+        return newImageUrl; // Resolve with the new image URL
+    } catch (error) {
+        console.error("Failed to update the image:", error);
+        throw error; // Propagate the error to the caller
+    }
 }
 
