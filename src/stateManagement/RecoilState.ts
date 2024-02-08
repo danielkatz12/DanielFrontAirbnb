e10abbadbfd0683ce06c502daf99857445c3dd78
danielkatz12 <danielkatz1212@gmail.com>
2024-02-08 23:22:54 +0200

    changing the state and adding full post state

--- src/stateManagement/RecoilState.ts
+++ src/stateManagement/RecoilState.ts
@@ -1,51 +1,34 @@
-import { atom } from 'recoil';
-import { ReactElement } from 'react';
+import {atom} from 'recoil';
+import {ReactElement} from 'react';
 import {
     getAccessTokenFromLocalStorage,
     getIdTokenFromLocalStorage,
     getRefreshTokenFromLocalStorage
 } from "../services/token-service.ts";
+import {PostItemData} from "../components/PostItem.tsx";
+
 
 // Interface for UserDetails
 export interface UserDetailsData {
     name: string;
-    userId?: string
+    user?: string
     contactEmail?: string;
-    contactPhoneNumber?: string;
+    contactPhoneNumber?: number;
     age?: number;
     profileImage?: string;
 }
-
-// // Interface for Tokens
-// export interface Tokens {
-//     accessToken?: string;
-//     refreshToken?: string;
-//     idToken?: string;
-// }
-
-
 
 // Atom for UserDetails
 export const userDetailsState = atom<UserDetailsData>({
     key: 'userDetailsState',
     default: {
         name: '',
-        userId: '',
+        user: '',
         contactEmail: undefined,
         contactPhoneNumber: undefined,
         age: undefined
     }
 });
-
-// // Atom for Tokens
-// export const tokensState = atom<Tokens>({
-//     key: 'tokensState',
-//     default: {
-//         accessToken: getAccessTokenFromLocalStorage() ?? undefined,
-//         refreshToken: getRefreshTokenFromLocalStorage() ?? undefined,
-//         idToken: getIdTokenFromLocalStorage() ?? undefined
-//     }
-// });
 
 
 export const accessTokenState = atom<string | undefined>({
@@ -67,4 +50,30 @@ export const idTokenState = atom<string | undefined>({
 export const currentDisplayedComponentState = atom<ReactElement | null>({
     key: 'currentDisplayedComponentState',
     default: null
+});
+export const fullPostsState = atom<PostItemData[]>({
+    key: 'fullPostsState',
+    default: [
+        {
+            _id: "65c3e6ea029f42b7e026c848",
+            city: "New York",
+            street: "Broadway",
+            streetNumber: 123,
+            description: "Spacious apartment in the heart of the city",
+            imageUrl: "http://example.com/image.jpg",
+            pricePerDay: 150,
+            user: {
+                _id: "65c35915d41fa5c482a9034c",
+                email: "danielkatz1212@gmail.com",
+                userProfileDetails: {
+                    name: "John Doe",
+                    profileImage: "http://example.com/profile.jpg",
+                    contactEmail: "john.doe@example.com",
+                    contactPhoneNumber: 1234567890,
+                    age: 30
+                }
+            },
+            "reviewCounts": 1
+        }
+    ]
 });
