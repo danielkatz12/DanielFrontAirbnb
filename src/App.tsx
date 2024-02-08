b945afbfc93684f092f2299c76f3495ec44710fb
danielkatz12 <danielkatz1212@gmail.com>
2024-02-08 23:26:53 +0200

    change logic in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,49 +5,89 @@ import {
     accessTokenState,
     currentDisplayedComponentState,
     idTokenState,
-    refreshTokenState
+    refreshTokenState,
+    userDetailsState
 } from "./stateManagement/RecoilState.ts";
 import {
-    deleteAccessTokenFromLocalStorage,
-    deleteIdTokenFromLocalStorage,
-    deleteRefreshTokenFromLocalStorage,
+    clearAllFromLocalStorage,
+    getAccessTokenFromLocalStorage,
+    getUserIDFromLocalStorage,
     saveAccessTokenInLocalStorage,
     saveIdTokenInLocalStorage,
     saveRefreshTokenInLocalStorage
 } from "./services/token-service.ts";
+import PostsList from "./components/PostsList.tsx";
+import PostForm from "./components/PostForm.tsx";
+import MyProfile from "./components/MyProfile.tsx";
+import {getMyUserProfileDetails} from "./services/user-profile-details-service.ts";
 
 
 function App() {
     // const [connectedUserDetails, setConnectedUserDetails] = useState<RegistrationDetails>();
-    const [currDisplayedComp] = useRecoilState(currentDisplayedComponentState);
+    const [currDisplayedComp, setCurrDisplayedComp] = useRecoilState(currentDisplayedComponentState);
+    const [userProfileDetails, setUserProfileDetails] = useRecoilState(userDetailsState);
+    const [accessToken, setAccessToken] = useRecoilState(accessTokenState);
+    const [refreshToken,setRefreshToken] = useRecoilState(refreshTokenState);
+    const [idToken,setIdToken] = useRecoilState(idTokenState);
 
-    const [accessToken] = useRecoilState(accessTokenState);
-    const [refreshToken] = useRecoilState(refreshTokenState);
-    const [idToken] = useRecoilState(idTokenState);
 
-
-    const isUserConnected = () => {
-        return accessToken;
+    const logout = () => {
+        clearAllFromLocalStorage();
+        setAccessToken(undefined);
+        setRefreshToken(undefined);
+        setIdToken(undefined);
+        alert("user is now Logout")
     }
 
     //TODO: MOVE TO OTHER PLACE THIS LOGIC!!
     useEffect(() => {
-        accessToken ? saveAccessTokenInLocalStorage(accessToken) : deleteAccessTokenFromLocalStorage();
+        accessToken && saveAccessTokenInLocalStorage(accessToken);
     }, [accessToken]);
 
     useEffect(() => {
-        refreshToken ? saveRefreshTokenInLocalStorage(refreshToken) : deleteRefreshTokenFromLocalStorage();
+        refreshToken && saveRefreshTokenInLocalStorage(refreshToken);
     }, [refreshToken]);
 
     useEffect(() => {
-        idToken ? saveIdTokenInLocalStorage(idToken) : deleteIdTokenFromLocalStorage();
+        idToken && saveIdTokenInLocalStorage(idToken);
     }, [idToken]);
+    useEffect(() => {
+        getAccessTokenFromLocalStorage() && getUserIDFromLocalStorage() &&
+        getMyUserProfileDetails(getUserIDFromLocalStorage()!)
+            .then((value) => setUserProfileDetails(value))
+            .catch((error) => (console.log("failed to gey the uder details from server")))
+    }, []);
 
+    useEffect(() => {
+        setCurrDisplayedComp(<PostsList/>)
+    }, []);
 
     return (
         <div>
-            {isUserConnected() && currDisplayedComp}
-            {!isUserConnected() && <Registration/>}
+            <div className="d-flex justify-content-center position-relative">
+                {accessToken &&
+                    <div>
+                        <button type="button" onClick={() => setCurrDisplayedComp(<PostForm/>)}
+                                className="btn btn btn-primary">Add Post
+                        </button>
+                        <button type="button"
+                                onClick={() => setCurrDisplayedComp(<MyProfile userProfileDetails={userProfileDetails}/>)}
+                                className="btn btn btn-primary">My Profile
+                        </button>
+                        <button type="button" onClick={logout}
+                                className="btn btn btn-outline-danger">Logout
+                        </button>
+                    </div>
+                }
+                {(!accessToken) &&
+                    <button type="button" onClick={() => setCurrDisplayedComp(<Registration/>)}
+                            className="btn btn btn-primary">Registration
+                    </button>}
+            </div>
+            <div>
+                {currDisplayedComp}
+
+            </div>
 
         </div>
     )
