cf233e32cdaeca521edf3eb832fdb3554aef0333
danielkatz12 <danielkatz1212@gmail.com>
2024-03-12 23:53:34 +0200

    config changes, added deletePostById to post service

--- src/services/posts-service.ts
+++ src/services/posts-service.ts
@@ -1,10 +1,8 @@
-import apiClient, {axiosConfig, CanceledError} from "./api-client"
+import apiClient from "./api-client"
 
 import {PostItemData} from "../components/PostItem.tsx"
 import {PostDto} from "../dtos/post-dto.ts";
-import {UserDetailsData} from "../stateManagement/RecoilState.ts";
 
-export {CanceledError}
 
 export function getAllFullPosts() {
     const abortController = new AbortController()
@@ -16,7 +14,7 @@ export function insertPost(postDto: PostDto): Promise<void> {
     return new Promise<void>((resolve, reject) => {
         console.log("inserting new post")
         console.log(postDto)
-        apiClient.post("/post", postDto, axiosConfig).then((response) => {
+        apiClient.post("/post", postDto).then((response) => {
             console.log("Post saved successfully: ", response)
             resolve();
         }).catch((error) => {
@@ -29,12 +27,25 @@ export function insertPost(postDto: PostDto): Promise<void> {
 export function updatePost(postDto: PostDto): Promise<PostDto> {
     return new Promise<PostDto>((resolve, reject) => {
         console.log("Updating post: ", postDto);
-        apiClient.put(`/post/${postDto._id}`, postDto, axiosConfig).then((response) => {
+        apiClient.put(`/post/${postDto._id}`, postDto).then((response) => {
             console.log("Post saved successfully: ", response)
             resolve(response.data);
         }).catch((error) => {
             console.log("failed to update Post: ", error)
             reject(error)
+        })
+    })
+}
+
+export function deletePostById(postId: string): Promise<PostDto> {
+    return new Promise<PostDto>((resolve, reject) => {
+        console.log("deleting post with id: ", postId);
+        apiClient.delete(`/post/${postId}`).then((response) => {
+            console.log("Post deleted successfuly: ", response);
+            resolve(response.data);
+        }).catch((error) => {
+            console.log("Failed to delete Post: ", error);
+            reject(error);
         })
     })
 }
