e9a77b2ae62e0b5872e9cdc508809f73fff3a62a
danielkatz12 <danielkatz1212@gmail.com>
2024-03-12 22:13:54 +0200

    refresh token logic: added interceptor to responses. logout logic.

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,25 +1,105 @@
-import axios, {AxiosRequestConfig, CanceledError} from "axios";
-import {getAccessTokenFromLocalStorage} from "./token-service.ts";
+import axios, {AxiosError, AxiosRequestConfig, AxiosResponse, InternalAxiosRequestConfig} from 'axios';
+import {
+    getAccessTokenFromLocalStorage,
+    getRefreshTokenFromLocalStorage, saveAccessTokenInLocalStorage,
+    saveRefreshTokenInLocalStorage
+} from "./token-service.ts";
+import {useRefreshToken} from "./user-service.ts";
 
-export {CanceledError}
-const apiClient = axios.create({
-    baseURL: 'http://localhost:3000',
-});
-
-// Define your header
 const headers = {
     'Content-Type': 'application/json' // example content type, adjust as needed
 };
 
-// Axios request configuration
-export const axiosConfig: AxiosRequestConfig = {
-    headers: headers
-};
+const apiClient = axios.create({
+    baseURL: 'http://localhost:3000',
+    headers: {...headers}
+});
+// // Add a request interceptor to attach access token to requests
+// apiClient.interceptors.request.use((config: AxiosRequestConfig) => {
+//     const token = getAccessTokenFromLocalStorage();
+//     if (token) {
+//         config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+// });
 
-apiClient.interceptors.request.use((config) => {
-    const token: string | null = getAccessTokenFromLocalStorage();
-    config.headers.Authorization = `Bearer ${token}`;
+// Define your header
+// Add a request interceptor to attach access token to requests
+apiClient.interceptors.request.use((config: InternalAxiosRequestConfig) => {
+    if (!authBypass(config)) {
+        const token: string | null = getAccessTokenFromLocalStorage();
+        config.headers.Authorization = `Bearer ${token}`;
+    }
     return config;
 });
+
+function authBypass(config: InternalAxiosRequestConfig) {
+    return config.url!.startsWith('/auth/refresh') ||  config.url!.startsWith('/auth/logout');
+}
+
+export { AxiosError }
+
+let isRefreshing = false;
+let failedQueue: ((token: string) => void)[] = [];
+
+// Add a response interceptor to handle token refreshing and retrying failed requests
+apiClient.interceptors.response.use(
+    (response: AxiosResponse) => {
+        return response;
+    },
+    async (error: AxiosError) => {
+        const originalRequest = error.config;
+        if (error.response?.status === 401 || error.response?.status === 403) {
+            console.log("got authorization error ->> need to refersh token");
+            if (!isRefreshing) {
+                isRefreshing = true;
+                //const refreshToken = getRefreshTokenFromLocalStorage();
+                try {
+                    // Call your server's refresh token endpoint
+                    await useRefreshToken();
+                    const newAccessToken = getAccessTokenFromLocalStorage();
+
+                    // Store the new access and refresh tokens
+                    // saveRefreshTokenInLocalStorage(response.data.refreshToken);
+                    // saveAccessTokenInLocalStorage(newAccessToken);
+
+                    //console.log("Finish to refresh the tokens and saved on local storage successfully.")
+
+                    // Retry all queued requests with the new access token
+                    retryFailedRequests(newAccessToken!);
+
+                    // Retry the original request with the new access token
+                    originalRequest!.headers.Authorization = `Bearer ${newAccessToken}`;//todo: maybe it is useless becoase we already have intercaptor on all request??
+                    return axios(originalRequest!);
+                } catch (refreshError) {
+                    // Handle refresh token failure (e.g., redirect to login)
+                    console.error('Error refreshing token:', refreshError);
+                    throw refreshError;
+                } finally {
+                    isRefreshing = false;
+                }
+            } else {
+                // If token is already refreshing, add the request to the failed queue
+                return new Promise((resolve, reject) => {
+                    console.log("still in refreshing tokens... saved the request to retry it later...")
+                    failedQueue.push((token: string) => {
+                        originalRequest!.headers.Authorization = `Bearer ${token}`;
+                        resolve(axios(originalRequest!));
+                    });
+                });
+            }
+        }
+        return Promise.reject(error);
+    }
+);
+
+
+// Function to retry all queued requests with the new access token
+const retryFailedRequests = (newAccessToken: string) => {
+    console.log("Retry failed request...")
+    failedQueue.forEach(callback => callback(getAccessTokenFromLocalStorage()!));
+    failedQueue = []; // Clear the failed queue after retrying all requests
+};
+
 
 export default apiClient;
--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,5 +1,10 @@
 import apiClient from "./api-client"
 import {CredentialResponse} from "@react-oauth/google";
+import {
+    getRefreshTokenFromLocalStorage,
+    saveAccessTokenInLocalStorage,
+    saveRefreshTokenInLocalStorage
+} from "./token-service.ts";
 
 export interface IUser {
     email: string,
@@ -9,12 +14,16 @@ export interface IUser {
     refreshToken?: string
 }
 
+export interface Tokens {
+    accessToken: string,
+    refreshToken: string
+}
 export const registerUser = (user: IUser) => {
     return new Promise<IUser>((resolve, reject) => {
         console.log("Registering user...")
         console.log(user)
         apiClient.post("/auth/register", user).then((response) => {
-            console.log(response)
+            console.log("User has been registered successfully")
             resolve(response.data)
         }).catch((error) => {
             console.log(error)
@@ -22,19 +31,20 @@ export const registerUser = (user: IUser) => {
         })
     })
 }
-// export const registerUser = (standardRegistration: StandardRegistration) => {
-//     return new Promise<User>((resolve, reject) => {
-//         console.log("Registering user...")
-//         console.log(standardRegistration)
-//         apiClient.post("/auth/register", standardRegistration).then((response) => {
-//             console.log(response)
-//             resolve(response.data)
-//         }).catch((error) => {
-//             console.log(error)
-//             reject(error)
-//         })
-//     })
-// }
+
+export const loginUser = (user: IUser) => {
+    return new Promise<IUser>((resolve, reject) => {
+        console.log("Login user request to server... user: ", user);
+        apiClient.post("/auth/login", user).then((response) => {
+            console.log("User is login successfully")
+            resolve(response.data);
+        }).catch((error) => {
+            console.log("Failed to login the user");
+            reject(error);
+        })
+
+    })
+}
 
 export const googleSignin = (credentialResponse: CredentialResponse) => {
     return new Promise<IUser>((resolve, reject) => {
@@ -44,6 +54,46 @@ export const googleSignin = (credentialResponse: CredentialResponse) => {
             resolve(response.data)
         }).catch((error) => {
             console.log(error)
+            reject(error)
+        })
+    })
+}
+
+export function useRefreshToken(): Promise<Tokens> {
+
+    return new Promise<Tokens>((resolve, reject) => {
+        console.log("about to get new refresh and access token...")
+        apiClient.get('/auth/refresh', {
+            headers: {
+                'Authorization': `Bearer ${getRefreshTokenFromLocalStorage()}`
+            }
+        }).then(response => {
+            console.log("refresh tokens successfully!")
+
+            saveRefreshTokenInLocalStorage(response.data.refreshToken);
+            saveAccessTokenInLocalStorage(response.data.accessToken);
+            console.log("saved new tokens in local storage..")
+            console.log(response)
+            resolve(response.data)
+        }).catch((error) => {
+            console.log(error)
+            reject(error)
+        })
+    })
+}
+
+export function logoutFromServer(): Promise<void>{
+    return new Promise<void>((resolve, reject) => {
+        console.log("about to logout from server...");
+        apiClient.get('/auth/logout', {
+            headers: {
+                'Authorization': `Bearer ${getRefreshTokenFromLocalStorage()}`
+            }
+        }).then(response => {
+            console.log("User logout successfully!")
+            resolve(response.data)
+        }).catch((error) => {
+            console.log("Failed to logout... error: ",error)
             reject(error)
         })
     })
