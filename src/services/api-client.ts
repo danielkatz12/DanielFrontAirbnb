998632804cb8c026bc3cf3ccef47091c6f4dc5d1
danielkatz12 <danielkatz1212@gmail.com>
2024-03-17 16:53:58 +0200

    prod

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -11,7 +11,7 @@ const headers = {
 };
 
 const apiClient = axios.create({
-    baseURL: 'https://193.106.55.228',
+    baseURL: 'https://10.10.248.228',
     headers: {...headers}
 });
 // // Add a request interceptor to attach access token to requests
